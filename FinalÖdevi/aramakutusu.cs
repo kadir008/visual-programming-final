77a4b85836d4c3202acbfd9f2864784a9f0bace5
kadir008 <aslankral216@gmail.com>
2023-01-03 21:01:39 +0300

    ilk yükleme

---
+++ FinalÖdevi/aramakutusu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalÖdevi
+{
+    public class aramakutusu
+    {
+        public int Sira { get; set; }
+        public string MuzikName { get; set; }
+        public string MuzikUrl { get; set; }
+        public string MuzikSanatci { get; set; }
+        public bool RadyoMu { get; set; }
+    }
+}
